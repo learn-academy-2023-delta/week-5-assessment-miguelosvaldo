d7225335de95be8377fa691a7b6d06be95da8e9d
miguelosvaldo <miguel.o.molina@icloud.com>
2023-06-10 18:10:26 -0700

    week 5 brain dead

--- interview-questions.md
+++ interview-questions.md
@@ -8,44 +8,44 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What does calling super do in a Ruby class?
 
-Your answer:
+Your answer: Calling super will identify a previous method and call on it.
 
-Researched answer:
+Researched answer: In ruby, it is used to invoke a method with the same name.
 
 2. What is a gem?
 
-Your answer:
+Your answer: A gem is a ruby program to run database code.
 
-Researched answer:
+Researched answer: A package of code that will give you access to custom methods wrote by someone else.
 
 3. What is a relational database? Are there other kinds of databases?
 
-Your answer:
+Your answer: A relational data base is a databse that could use other programs to run. yes.
 
-Researched answer:
+Researched answer: Relational database is a program used to create, update, and manage databases.
 
 4. What are primary keys? Why purpose does a primary key serve?
 
-Your answer:
+Your answer: Primary Keys are identification of certain entries in a database. They serve the purpose of attaching unique instances of data, and to group other sub keys to them.
 
-Researched answer:
+Researched answer: To extract or archive data from a table that is visited more than once during a process
 
 5. What are the five HTTP verbs? What is each verb's corresponding CRUD action?
 
-Your answer:
+Your answer: The five verbs are post, get, put, patch, and delete
 
-Researched answer:
+Researched answer: The primary or most-commonly-used HTTP verbs (or methods, as they are properly called) are POST, GET, PUT, PATCH, and DELETE. These correspond to create, read, update, and delete (or CRUD) operations, respectively.
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. Model validations:
+1. Model validations: the process of confirming that the model actually achieves its intended purpose
 
-2. RESTful routes:
+2. RESTful routes:  a route that provides a mapping of HTTP verbs ( get , post , put , patch , delete ) to the CRUD actions ( create , read , update, delete ) that are defined in our controllers
 
-3. ERB:
+3. ERB: returns a string in the same character encoding as the input string
 
-4. Params:
+4. Params: By using the params keyword, you can specify a method parameter that takes a variable number of arguments
 
-5. API:
+5. API: application programming interface
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -9,7 +9,12 @@ filter_letter_o = 'o'
 # Expected output: ['coffee', 'soda water']
 filter_letter_t = 't'
 # Expected output: ['tea', 'water', 'soda water']
-
+ 
+def particular (words, letter) 
+    words.select { |word| word.include?(letter) }
+end
+p particular(beverages_array, filter_letter_o)
+p particular(beverages_array, filter_letter_t)
 
 # -------------------2) Create a method that takes in a hash and returns one array with all the hash values at their own index and in alphabetical order. No nested arrays. Use the test variable provided.
 # HINT: Google 'ruby get rid of nested arrays'
@@ -17,11 +22,41 @@ filter_letter_t = 't'
 us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], notheast: ['Maine', 'New Hampshire', 'Rhode Island'] }
 # Expected output: ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Rhode Island', 'Washington'] 
 
+def hash_values (hash)
+    hash.values.flatten.sort
+end
+
+p hash_values(us_states)
 
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
 
-# Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
+class Bike 
+    def initialize(model, wheels = 2, current_speed = 0)
+        @model = model
+        @wheels = wheels
+        @current_speed = current_speed
+    end
+    def bike_info
+         "The bike model is #{@model}. It has #{@wheels} wheels and its current speed is #{@current_speed}."
+    end 
+    def pedal_faster(speed_increase)
+        @current_speed += speed_increase
+      end
+    
+      def brake(speed_decrease)
+        @current_speed -= speed_decrease
+        @current_speed > 0 ? @current_speed :0
+      end
+end
 
+p my_bike = Bike.new('Trek Bike')
+p my_bike.bike_info
+
+# Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
+p my_bike.pedal_faster(10) => 10
+p my_bike.pedal_faster(18) => 28
+p my_bike.brake(5) => 23
+p my_bike.brake(25) => 0
 
 
 # -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
